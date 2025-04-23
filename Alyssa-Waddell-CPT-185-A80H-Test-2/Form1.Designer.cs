cf5e73279d14d0c13834bd2aa1fa3cfeecdcf467
koburai <kugaspam@gmail.com>
2025-04-23 10:12:57 -0400

    Fall 2024 C# Test 2 - Read and Write

---
+++ Alyssa-Waddell-CPT-185-A80H-Test-2/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Alyssa_Waddell_CPT_185_A80H_Test_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnProc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxYear = new System.Windows.Forms.ComboBox();
+            this.ttYear = new System.Windows.Forms.ToolTip(this.components);
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblSalTotal = new System.Windows.Forms.Label();
+            this.lblRecNum = new System.Windows.Forms.Label();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.lblLargest = new System.Windows.Forms.Label();
+            this.lblSmallest = new System.Windows.Forms.Label();
+            this.lblBonusAmt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProc
+            // 
+            this.btnProc.AutoSize = true;
+            this.btnProc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnProc.Location = new System.Drawing.Point(16, 39);
+            this.btnProc.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnProc.Name = "btnProc";
+            this.btnProc.Size = new System.Drawing.Size(157, 30);
+            this.btnProc.TabIndex = 1;
+            this.btnProc.Text = "&Process the file ->";
+            this.btnProc.UseVisualStyleBackColor = true;
+            this.btnProc.Click += new System.EventHandler(this.btnProc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnExit.Location = new System.Drawing.Point(16, 102);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(47, 30);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnClear.Location = new System.Drawing.Point(114, 102);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 30);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(249, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Year:";
+            // 
+            // cboxYear
+            // 
+            this.cboxYear.FormattingEnabled = true;
+            this.cboxYear.Location = new System.Drawing.Point(301, 71);
+            this.cboxYear.Name = "cboxYear";
+            this.cboxYear.Size = new System.Drawing.Size(121, 28);
+            this.cboxYear.TabIndex = 5;
+            this.ttYear.SetToolTip(this.cboxYear, "Select a year to pull from.");
+            // 
+            // lblSalTotal
+            // 
+            this.lblSalTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSalTotal.Location = new System.Drawing.Point(172, 180);
+            this.lblSalTotal.Name = "lblSalTotal";
+            this.lblSalTotal.Size = new System.Drawing.Size(329, 27);
+            this.lblSalTotal.TabIndex = 6;
+            // 
+            // lblRecNum
+            // 
+            this.lblRecNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRecNum.Location = new System.Drawing.Point(172, 217);
+            this.lblRecNum.Name = "lblRecNum";
+            this.lblRecNum.Size = new System.Drawing.Size(329, 27);
+            this.lblRecNum.TabIndex = 7;
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAvg.Location = new System.Drawing.Point(172, 256);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.Size = new System.Drawing.Size(329, 27);
+            this.lblAvg.TabIndex = 8;
+            // 
+            // lblLargest
+            // 
+            this.lblLargest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLargest.Location = new System.Drawing.Point(172, 295);
+            this.lblLargest.Name = "lblLargest";
+            this.lblLargest.Size = new System.Drawing.Size(329, 27);
+            this.lblLargest.TabIndex = 9;
+            // 
+            // lblSmallest
+            // 
+            this.lblSmallest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSmallest.Location = new System.Drawing.Point(172, 334);
+            this.lblSmallest.Name = "lblSmallest";
+            this.lblSmallest.Size = new System.Drawing.Size(329, 27);
+            this.lblSmallest.TabIndex = 10;
+            // 
+            // lblBonusAmt
+            // 
+            this.lblBonusAmt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBonusAmt.Location = new System.Drawing.Point(172, 375);
+            this.lblBonusAmt.Name = "lblBonusAmt";
+            this.lblBonusAmt.Size = new System.Drawing.Size(329, 27);
+            this.lblBonusAmt.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnProc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(536, 418);
+            this.Controls.Add(this.lblBonusAmt);
+            this.Controls.Add(this.lblSmallest);
+            this.Controls.Add(this.lblLargest);
+            this.Controls.Add(this.lblAvg);
+            this.Controls.Add(this.lblRecNum);
+            this.Controls.Add(this.lblSalTotal);
+            this.Controls.Add(this.cboxYear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnProc);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alyssa Waddell CPT 185 Test 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnProc;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboxYear;
+        private System.Windows.Forms.ToolTip ttYear;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label lblSalTotal;
+        private System.Windows.Forms.Label lblRecNum;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.Label lblLargest;
+        private System.Windows.Forms.Label lblSmallest;
+        private System.Windows.Forms.Label lblBonusAmt;
+    }
+}
+
---
+++ Alyssa-Waddell-CPT-185-A80H-Test-2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO; // had to add this in
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Deployment.Application;
+
+namespace Alyssa_Waddell_CPT_185_A80H_Test_2
+{
+    public partial class Form1 : Form
+    {
+        /* alyssa waddell
+         * cpt 185 a80h
+         * test 2 
+         */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamReader inputFile;// put this up here just because. yeah.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // i plan on populating the combo box this way as manually typing in every single year between 1900 and 2024 is yike
+            for (int i = 1900; i <= 2024; i++)
+            {
+                cboxYear.Items.Add(i);
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblAvg.Text = string.Empty;
+            lblBonusAmt.Text = string.Empty;
+            lblLargest.Text = string.Empty;
+            lblSmallest.Text = string.Empty;
+            lblRecNum.Text = string.Empty;
+            lblSalTotal.Text = string.Empty;
+            cboxYear.SelectedIndex = -1;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); // close
+        }
+
+        private void btnProc_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                // if the user DOES select a file
+                // open the file
+                inputFile = File.OpenText(openFile.FileName); // assign the file name to inputFile
+                // during troubleshooting i used the dummydata.txt file you can find in this folder
+            }
+            else
+            {
+                // if they do anything else
+                MessageBox.Show("Cancelled.");
+                return; // end method early so nothing breaks
+            }
+            double[] lines = Array.ConvertAll(File.ReadAllLines(openFile.FileName), double.Parse); // read the file. update fileName. parse to array
+            int lineAmt = lines.Length; // get the amt of lines/values. this helps it so it can run with however many records are present
+            lblRecNum.Text = "The total number of records: " + lineAmt;
+            // now to actually. get the data
+            for (int i = 0; i < lineAmt; i++)
+            {
+                double data = lines[i]; // populates the array with the pulled data
+                // Console.WriteLine(data); leftover from making sure it pulled
+            }
+            minMaxSal(lines); // pass the lines data to the method
+            getAvg(lines); // calc average and bonus
+            inputFile.Close(); // close that file
+        }
+
+        private void getAvg(double[] data)
+        {
+            double sum = 0.0;  // Declare sum outside the loop
+
+            foreach (double num in data)
+            {
+                sum += num;  // Add each element to sum
+            }
+            lblSalTotal.Text = "The total salaries: "+ sum.ToString("c"); // display total to label
+
+            // Calculate the Bonus if the combo box year is less than 2000 for 10 % of the average, 20 % if the box is in between 2000 and 2008, otherwise it’s 0.0
+            double avg = 0.0;
+            if (cboxYear.SelectedItem != null)
+            {
+                int.TryParse(cboxYear.SelectedItem.ToString(), out int yearPick); // assign chosen year as yearPick
+                if (yearPick < 2000) // less than 2000
+                {
+                    avg = sum / data.Length; // calc average
+                    lblAvg.Text = "The average: " + avg.ToString("c"); // display average to label
+                    double bonusPercentage = 0.0;
+
+                    if (yearPick >= 2000 && yearPick <= 2008)
+                    {
+                        bonusPercentage = 0.2; // between 2000 and 2008
+                    }
+                    else if (yearPick < 2000)
+                    {
+                        bonusPercentage = 0.1; // before 2000
+                    }
+                    lblBonusAmt.Text = "The Bonus amount is: " + (avg * bonusPercentage).ToString("c");
+                }
+                else
+                {
+                    lblBonusAmt.Text = "No Bonus available."; // in case there's extraneuous values
+                }
+            }
+            else // if they don't select a year
+            {
+                lblAvg.Text = string.Empty;
+                lblBonusAmt.Text = string.Empty;
+                lblLargest.Text = string.Empty;
+                lblSmallest.Text = string.Empty;
+                lblRecNum.Text = string.Empty;
+                lblSalTotal.Text = string.Empty;
+                cboxYear.SelectedIndex = -1; // clear everything out
+                MessageBox.Show("Please select a year.");
+                
+            }
+        }
+
+        private void minMaxSal(double[] data) // pull the array. name here doesn't matter i think??
+        {
+            // Create a void Method to call to find minimum or maximum salaries and display them.	
+            double minSalary = double.MaxValue;
+            double maxSalary = double.MinValue;
+
+            foreach (double salary in data) // salary is basically 'i' here. data is the array
+            {
+                if (salary < minSalary)
+                    minSalary = salary; // update if its the smallest
+                lblSmallest.Text = "The smallest salary: " + minSalary.ToString("c"); // update label
+
+                if (salary > maxSalary)
+                    maxSalary = salary; // same as above but ofr max
+                lblLargest.Text = "The largest salary: " + maxSalary.ToString("c"); // same as above but for max
+            }
+        }
+        // my head hurts.
+    }
+}
